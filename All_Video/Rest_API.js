9b85341a59a8943f1264b51314c72601da37dbe7
kevinpatel-2205 <kevinpatel37592@gmail.com>
2024-09-16 23:59:19 +0530

    today learn about header what  is use of header

--- All_Video/Rest_API.js
+++ All_Video/Rest_API.js
@@ -27,23 +27,32 @@ const app = express();
 //     return res.json({status:"pending"});
 // });
 
-app
-  .route("/api/users/:id")
-  .get((req, res) => {
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id);
-    return res.json(user);
-  })
-  .patch((req, res) => {
-    // edit user with id
-    return res.json({ status: "pending" });
-  })
-  .delete((req, res) => {
-    // edit user with id
-    return res.json({ status: "pending" });
-  })
-  .post((req, res) => {
-    return res.json({ status: "pending" });
-  });
+// app
+//   .route("/api/users/:id")
+//   .get((req, res) => {
+//     const id = Number(req.params.id);
+//     const user = users.find((user) => user.id === id);
+//     return res.json(user);
+//   })
+//   .patch((req, res) => {
+//     // edit user with id
+//     return res.json({ status: "pending" });
+//   })
+//   .delete((req, res) => {
+//     // edit user with id
+//     return res.json({ status: "pending" });
+//   })
+//   .post((req, res) => {
+//     return res.json({ status: "pending" });
+//   });
+
+
+// we can set and retrive header
+app.get("/api/users",(req,res)=>{
+  res.setHeader("myName","Kevin Patel");
+  console.log(req.headers);
+  return res.json(users);
+});
+
 
 app.listen(8000, () => console.log("server is start !"));
